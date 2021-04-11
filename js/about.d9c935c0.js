04155bf537d1a14c7be19a01a619457ef42106ee
Isti115 <Isti115@users.noreply.github.com>
2021-04-11 12:30:15 +0000

    deploy: a81df62bfe8ab6487b7df9a1866ef1fc21d6d461

--- js/about.d9c935c0.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var c=a("7a23");const e={class:"about"},o=Object(c["g"])("h1",null,"This is an about page",-1);function s(n,t){return Object(c["p"])(),Object(c["d"])("div",e,[o])}const u={};u.render=s;t["default"]=u}}]);
-//# sourceMappingURL=about.d9c935c0.js.map
