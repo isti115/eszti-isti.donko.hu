31a26e014b93f5e557c64fea05a86fa83acf9fd8
Isti115 <Isti115@users.noreply.github.com>
2021-04-15 16:07:37 +0000

    deploy: f3f51ee088c40bcc0c86c50f21ba2fc9c8aad5b0

--- js/about.2f8acbab.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,o){"use strict";o.r(t);var a=o("7a23");const c={class:"about"},e=Object(a["f"])("h1",null,"This is an about page",-1);function s(n,t){return Object(a["o"])(),Object(a["d"])("div",c,[e])}const u={};u.render=s;t["default"]=u}}]);
-//# sourceMappingURL=about.2f8acbab.js.map
