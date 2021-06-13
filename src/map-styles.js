8a5b7ab1ffc64fe1e03f30754f83ac6bdb316de4
Donkó István <istvan.donko@gmail.com>
2021-06-13 14:57:55 +0200

    Update map visuals

---
+++ src/map-styles.js
@@ -0,0 +1,156 @@
+export default [
+  {
+    elementType: 'geometry',
+    stylers: [{ color: '#e9e4d8' }]
+  },
+  {
+    elementType: 'labels.icon',
+    stylers: [{ saturation: -100 }]
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#523735' }]
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [{ color: '#f5f1e6' }]
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.stroke',
+    stylers: [{ color: '#c9b2a6' }]
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'geometry',
+    stylers: [{ color: '#ad9d7f' }]
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'geometry.stroke',
+    stylers: [{ color: '#857960' }]
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#ae9e90' }]
+  },
+  {
+    featureType: 'landscape.natural',
+    elementType: 'geometry',
+    stylers: [{ color: '#d5cdb4' }, { saturation: -40 }]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [{ color: '#dfd2ae' }, { saturation: -75 }]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#93817c' }]
+  },
+  {
+    featureType: 'poi.business',
+    stylers: [{ visibility: 'simplified' }]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry.fill',
+    stylers: [{ color: '#c0cc8a' }, { saturation: -60 }]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text',
+    stylers: [{ visibility: 'off' }]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#447530' }]
+  },
+  {
+    featureType: 'road',
+    stylers: [{ saturation: -50 }, { lightness: 10 }, { visibility: 'simplified' }]
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [{ color: '#f5f1e6' }]
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.icon',
+    stylers: [{ saturation: -70 }, { visibility: 'off' }]
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [{ color: '#fdfcf8' }]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [{ color: '#a49a84' }]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [{ color: '#abb97c' }]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [{ color: '#8f7f61' }, { saturation: -100 }]
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry',
+    stylers: [{ lightness: -30 }]
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry.stroke',
+    stylers: [{ color: '#db8555' }]
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#806b63' }]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'geometry',
+    stylers: [{ color: '#dfd2ae' }]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#8f7d77' }]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'labels.text.stroke',
+    stylers: [{ color: '#ebe3cd' }]
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'geometry',
+    stylers: [{ color: '#dfd2ae' }, { saturation: -75 }]
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'labels.icon',
+    stylers: [{ saturation: -75 }]
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry.fill',
+    stylers: [{ color: '#b9d3c2' }]
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#92998d' }]
+  }
+]
--- src/slides/Party.vue
+++ src/slides/Party.vue
@@ -2,26 +2,48 @@
   <div id="party" class="slide location">
     <GoHome />
     <h2>Anna Tanya</h2>
-    <Map query="Anna+Tanya,Törökbálint" />
+    <Map :options="mapOptions" />
   </div>
 </template>
 
 <script>
 import GoHome from '@/components/GoHome.vue'
 import Map from '@/components/Map.vue'
+import navIcon from '@/nav-icon.js'
 
 export default {
   name: 'Party',
   components: {
     GoHome,
     Map
-  }
+  },
+  data: () => ({
+    mapOptions: {
+      center: {
+        lat: 47.426103,
+        lng: 18.933639
+      },
+      zoom: 16,
+      icon: 'party',
+      content: `
+        <h3>Anna Tanya</h3>
+        <p>Törökbálint, Hegyalja u. ???., 2045</p>
+        <a
+          class="navlink"
+          href="https://www.google.com/maps/dir//Törökbálint,+Anna+Tanya"
+          target="_blank"
+        >
+          ${navIcon}
+        </a>
+      `
+    }
+  })
 }
 </script>
 
 <style scoped>
 #party {
-background-color: rgb(226,229,222);
+  background-color: rgb(226,229,222);
 }
 
 .location > div {
